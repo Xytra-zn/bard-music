5009b29598bc164c79ecff3fbaa49b5c980d9a73
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-09 17:46:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from Commands import Commands
+from Genre import Genre
+from Music_player import MusicPlayer
 
 app = Client(
     "Music Bot",
@@ -8,16 +12,53 @@ app = Client(
     bot_token=os.environ["BOT_TOKEN"]
 )
 
-from Music_player import MusicPlayer
-from Commands import Commands
-
 music_player = MusicPlayer(app)
 commands = Commands(app, music_player)
+genre = Genre(app)
 
-@app.on_message()
-def handle_message(client, message):
-    commands.handler(message)
+# /start command
+@app.on_message(filters.command("start"))
+def start(client, message):
+    client.send_message(
+        message.chat.id,
+        "Welcome to the Music Bot! Use /help to see available commands."
+    )
+
+# /help command
+@app.on_message(filters.command("help"))
+def help(client, message):
+    client.send_message(
+        message.chat.id,
+        "Available commands:\n"
+        "/start - Start the bot\n"
+        "/help - Display this help message\n"
+        "/play - Play a song\n"
+        "/pause - Pause the current song\n"
+        "/resume - Resume the current song\n"
+        "/skip - Skip the current song\n"
+        "/queue - Show the current queue\n"
+        "/playlist - Create a new playlist\n"
+        "/add_to_playlist - Add a song to the playlist\n"
+        "/remove_from_playlist - Remove a song from the playlist\n"
+        "/get_playlist - Get the playlist\n"
+        "/play_playlist - Play the playlist\n"
+        "/delete_playlist - Delete the playlist\n"
+        "/genre - Get available genres\n"
+        "/play_genre - Play songs based on genre"
+    )
+
+# /genre command
+@app.on_message(filters.command("genre"))
+def show_genres(client, message):
+    genre.get_genres()
+
+# Callback function for inline buttons
+@app.on_callback_query()
+def callback_query(client, callback_query):
+    query_data = callback_query.data
+    if query_data.startswith("play_genre_"):
+        selected_genre = query_data.replace("play_genre_", "")
+        genre.play_genre(selected_genre)
 
 if __name__ == "__main__":
     app.run()
-  
